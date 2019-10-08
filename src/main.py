91fa873b872e94c5939f7dfa64be6ba9c5b1a3bf
Spartadays <watly.mateusz@gmail.com>
2019-10-09 00:58:49 +0200

    added bme and dht libraries

--- src/main.py
+++ src/main.py
@@ -2,15 +2,20 @@
 try:
     import pms7003
     import sim7000e
+    import BME280
+    import dht
     from time import sleep
     import machine
-    from machine import Timer, Pin
+    from machine import Timer, Pin, I2C
 except ImportError as i_err:
     print(i_err)
 
 #-----OBJECTS:-----
 sensor = pms7003.PMS7003()  # UART 1 (rx: 21, tx: 22)
 sim = sim7000e.SIM7000E()   # UART 2 (rx: 16, tx: 17)
+i2c = I2C(sda=Pin(25), scl=Pin(26), freq=10000)  # i2c for bmp280
+bme = BME280.BME280(i2c=i2c) # bmp280 sensor (but using bme library)
+dht_s = dht.DHT22(Pin(4))
 
 timer_0 = Timer(0)
 timer_1 = Timer(1)
@@ -23,8 +28,10 @@ mosfet_pin = Pin(19, Pin.OUT)
 #-----GLOBAL:-----
 send_flag = False
 pms_flag = False
+bme_flag = False
+dht_flag = False
 
-thingspeak_fields = [-999, -999, -999, None, None, None, None, None]
+thingspeak_fields = [None, None, None, None, None, None, None, None]
 #--------------------
 
 #-----TIMERS INTERRUPTS (for PMS7003):-----
@@ -52,6 +59,22 @@ def handle_timer_2(timer_2):
     print("PMS: Sleep\n")
 #--------------------
 
+#-----TIMERS INTERRUPTS (other):-----
+def handle_timer_0(timer_0):
+    global thingspeak_fields
+    thingspeak_fields[0] = str(bme.temperature)
+    thingspeak_fields[1] = str(bme.pressure)
+    global bme_flag
+    bme_flag = True
+
+def handle_timer_3(timer_3):
+    dht_s.measure()
+    global thingspeak_fields
+    thingspeak_fields[2] = str(dht_s.humidity())
+    global dht_flag
+    dht_flag = True
+#--------------------
+
 #-----STARTUP CODE:-----
 mosfet_pin.value(1)  # power on sensor and sim module
 sleep(2) # wait for everything to stabilize
@@ -60,6 +83,9 @@ sensor.send_command("passive")
 timer_1.init(mode=Timer.ONE_SHOT, period=60000, callback=handle_timer_1)
 print("PMS: Wakeup\n")
 
+timer_0.init(mode=Timer.ONE_SHOT, period=10000, callback=handle_timer_0)
+timer_3.init(mode=Timer.ONE_SHOT, period=20000, callback=handle_timer_3)
+
 sim.power_on(echo=True)
 sim.connect_to_thingspeak(gsm_apn='internet')
 #--------------------
@@ -67,14 +93,15 @@ sim.connect_to_thingspeak(gsm_apn='internet')
 #-----MAIN LOOP:-----
 while True:
     sim.print_uart()
-    if pms_flag and not send_flag:
+    if pms_flag and bme_flag and dht_flag and not send_flag:
         print(thingspeak_fields)
         sim.send_to_thinspeak(api_key='BY3E4OY6MMTCFJLR', fields=thingspeak_fields)
         sim.disconnect_from_thingspeak()
         sim.power_off()
         send_flag = True
-    if pms_flag and send_flag:
+    if pms_flag and bme_flag and dht_flag and send_flag:
         mosfet_pin.value(0)
+       
         x = 3600000
         print("SLEEP for " + str(x/1000) +  "seconds")
         machine.deepsleep(x)
